de00dda57e6f5b3de9c666adca0b5a230364dc62
nuharaomesh <omeshnuhara4@gmail.com>
2024-10-27 11:07:36 +0530

    init project

---
+++ src/main/java/lk/ijse/pcms_B/PcmsBApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.pcms_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmsBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcmsBApplication.class, args);
+	}
+
+}
