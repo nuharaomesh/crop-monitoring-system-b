e3845dc7eb951903165e318d1a9391638b8589ef
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-10 12:55:22 +0530

    define modelMapper as a bean

--- src/main/java/lk/ijse/pcms_B/PcmsBApplication.java
+++ src/main/java/lk/ijse/pcms_B/PcmsBApplication.java
@@ -1,13 +1,17 @@
 package lk.ijse.pcms_B;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PcmsBApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PcmsBApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
 }
