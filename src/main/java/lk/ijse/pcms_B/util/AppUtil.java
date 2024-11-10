77a066054a1dc275883087524c413fa404501569
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-10 12:52:14 +0530

    add appUtil class for utilities

---
+++ src/main/java/lk/ijse/pcms_B/util/AppUtil.java
@@ -0,0 +1,18 @@
+package lk.ijse.pcms_B.util;
+
+import java.util.Base64;
+import java.util.UUID;
+
+public class AppUtil {
+    public static String genEquipmentID() {
+        return "EQUIPMENT-" + UUID.randomUUID();
+    }
+
+    public static String genFieldCode() {
+        return "FIELD_CODE-" + UUID.randomUUID();
+    }
+
+    public static String imgToBase64(byte[] byteImg) {
+        return Base64.getEncoder().encodeToString(byteImg);
+    }
+}
