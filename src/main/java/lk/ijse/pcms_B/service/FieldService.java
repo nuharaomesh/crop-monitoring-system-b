895c32ffd2f94732fd5d307f9e0c21980bab9d8b
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-10 12:55:46 +0530

    impl Field CRUD ops

--- src/main/java/lk/ijse/pcms_B/service/FieldService.java
+++ src/main/java/lk/ijse/pcms_B/service/FieldService.java
@@ -1,4 +1,14 @@
 package lk.ijse.pcms_B.service;
 
+import lk.ijse.pcms_B.dto.FieldStatus;
+import lk.ijse.pcms_B.dto.impl.FieldDTO;
+
+import java.util.List;
+
 public interface FieldService {
+    FieldStatus getField(String fieldCode);
+    List<FieldDTO> getAllFields();
+    void saveField(FieldDTO dto);
+    void updateField(String fieldCode, FieldDTO dto);
+    void deleteField(String fieldCode);
 }
--- src/main/java/lk/ijse/pcms_B/service/impl/FieldServiceImpl.java
+++ src/main/java/lk/ijse/pcms_B/service/impl/FieldServiceImpl.java
@@ -1,10 +1,76 @@
 package lk.ijse.pcms_B.service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.pcms_B.customStatusCode.SelectedFieldErrorCode;
+import lk.ijse.pcms_B.dao.FieldDAO;
+import lk.ijse.pcms_B.dto.FieldStatus;
+import lk.ijse.pcms_B.dto.impl.FieldDTO;
+import lk.ijse.pcms_B.entity.impl.Field;
+import lk.ijse.pcms_B.exception.DataPersistException;
+import lk.ijse.pcms_B.exception.FieldNotFoundException;
 import lk.ijse.pcms_B.service.FieldService;
+import lk.ijse.pcms_B.util.AppUtil;
+import lk.ijse.pcms_B.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class FieldServiceImpl implements FieldService {
+
+    @Autowired
+    private FieldDAO fieldDAO;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public FieldStatus getField(String fieldCode) {
+        if (fieldDAO.existsById(fieldCode)) {
+            Field selectedField = fieldDAO.getReferenceById(fieldCode);
+            return mapping.toFieldDTO(selectedField);
+        } else {
+            return new SelectedFieldErrorCode(2, "Selected Field not found!");
+        }
+    }
+
+    @Override
+    public List<FieldDTO> getAllFields() {
+        return mapping.getFieldDTOList(fieldDAO.findAll());
+    }
+
+    @Override
+    public void saveField(FieldDTO dto) {
+        dto.setFieldCode(AppUtil.genFieldCode());
+        Field savedField =
+                fieldDAO.save(mapping.toFieldEntity(dto));
+        if (savedField == null)
+            throw new DataPersistException("Field Not Saved!");
+    }
+
+    @Override
+    public void updateField(String fieldCode, FieldDTO dto) {
+        Optional<Field> tmpField = fieldDAO.findById(fieldCode);
+        if (!tmpField.isPresent()) {
+            throw new FieldNotFoundException("Field Not Found!");
+        } else {
+            tmpField.get().setFieldName(dto.getFieldName());
+            tmpField.get().setFieldLocation(dto.getFieldLocation());
+            tmpField.get().setFieldSize(dto.getFieldSize());
+            tmpField.get().setImg1(dto.getImg1());
+            tmpField.get().setImg2(dto.getImg2());
+        }
+    }
+
+    @Override
+    public void deleteField(String fieldCode) {
+        Optional<Field> tmpField = fieldDAO.findById(fieldCode);
+        if (!tmpField.isPresent()) {
+            throw new FieldNotFoundException("Field Not Found!");
+        } else {
+            fieldDAO.deleteById(fieldCode);
+        }
+    }
 }
