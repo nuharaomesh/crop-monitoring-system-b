4ea63d6cf88ac5d6dfda4cef7094eb0a7e232679
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-15 08:39:18 +0530

    impl Crop CRUD ops

--- src/main/java/lk/ijse/pcms_B/service/CropService.java
+++ src/main/java/lk/ijse/pcms_B/service/CropService.java
@@ -1,4 +1,14 @@
 package lk.ijse.pcms_B.service;
 
+import lk.ijse.pcms_B.dto.CropStatus;
+import lk.ijse.pcms_B.dto.impl.CropDTO;
+
+import java.util.List;
+
 public interface CropService {
+    CropStatus getCrop(String cropCode);
+    List<CropDTO> getAllCrops();
+    void saveCrop(CropDTO dto);
+    void updateCrop(String cropCode, CropDTO dto);
+    void deleteCrop(String cropCode);
 }
--- src/main/java/lk/ijse/pcms_B/service/impl/CropServiceImpl.java
+++ src/main/java/lk/ijse/pcms_B/service/impl/CropServiceImpl.java
@@ -1,10 +1,75 @@
 package lk.ijse.pcms_B.service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.pcms_B.customStatusCode.SelectedCropErrorCode;
+import lk.ijse.pcms_B.dao.CropDAO;
+import lk.ijse.pcms_B.dto.CropStatus;
+import lk.ijse.pcms_B.dto.impl.CropDTO;
+import lk.ijse.pcms_B.entity.impl.Crop;
+import lk.ijse.pcms_B.exception.CropNotFoundException;
+import lk.ijse.pcms_B.exception.DataPersistException;
 import lk.ijse.pcms_B.service.CropService;
+import lk.ijse.pcms_B.util.AppUtil;
+import lk.ijse.pcms_B.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class CropServiceImpl implements CropService {
+
+    @Autowired
+    private CropDAO cropDAO;
+    @Autowired
+    private Mapping mapping;
+
+    @Override
+    public CropStatus getCrop(String cropCode) {
+        if (cropDAO.existsById(cropCode)) {
+            Crop selectedCrop = cropDAO.getReferenceById(cropCode);
+            return mapping.toCropDTO(selectedCrop);
+        } else {
+            return new SelectedCropErrorCode(2, "Selected Crop not found!");
+        }
+    }
+
+    @Override
+    public List<CropDTO> getAllCrops() {
+        return mapping.toCropDTOList(cropDAO.findAll());
+    }
+
+    @Override
+    public void saveCrop(CropDTO dto) {
+        dto.setCropCode(AppUtil.genCropCode());
+        Crop savedCrop =
+                cropDAO.save(mapping.toCropEntity(dto));
+        if (savedCrop == null) throw new DataPersistException("Crop Not Saved!");
+    }
+
+    @Override
+    public void updateCrop(String cropCode, CropDTO dto) {
+        Optional<Crop> tmpCrop = cropDAO.findById(cropCode);
+        if (!tmpCrop.isPresent()) {
+            throw new CropNotFoundException("Crop Not Found!");
+        } else {
+            tmpCrop.get().setCropName(dto.getCropName());
+            tmpCrop.get().setCropScientificName(dto.getCropScientificName());
+            tmpCrop.get().setCropImg(dto.getCropImg());
+            tmpCrop.get().setCategory(dto.getCategory());
+            tmpCrop.get().setCropSeason(dto.getCropSeason());
+        }
+    }
+
+    @Override
+    public void deleteCrop(String cropCode) {
+        Optional<Crop> tmpCrop = cropDAO.findById(cropCode);
+        if (!tmpCrop.isPresent()) {
+            throw new CropNotFoundException("Field Not Found!");
+        } else {
+            cropDAO.deleteById(cropCode);
+        }
+    }
 }
