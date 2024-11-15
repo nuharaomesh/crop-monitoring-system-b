a48af04a3eba2a44fac400028b93ed5797200634
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-15 09:11:50 +0530

    impl vehicle mappings

--- src/main/java/lk/ijse/pcms_B/util/Mapping.java
+++ src/main/java/lk/ijse/pcms_B/util/Mapping.java
@@ -3,9 +3,11 @@ package lk.ijse.pcms_B.util;
 import lk.ijse.pcms_B.dto.impl.CropDTO;
 import lk.ijse.pcms_B.dto.impl.EquipmentDTO;
 import lk.ijse.pcms_B.dto.impl.FieldDTO;
+import lk.ijse.pcms_B.dto.impl.VehicleDTO;
 import lk.ijse.pcms_B.entity.impl.Crop;
 import lk.ijse.pcms_B.entity.impl.Equipment;
 import lk.ijse.pcms_B.entity.impl.Field;
+import lk.ijse.pcms_B.entity.impl.Vehicle;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,5 +54,17 @@ public class Mapping {
 
     public List<CropDTO> toCropDTOList(List<Crop> equipmentList) {
         return modelMapper.map(equipmentList, new TypeToken<List<CropDTO>>(){}.getType());
+    }
+
+    public Vehicle toVehicleEntity(VehicleDTO dto) {
+        return modelMapper.map(dto, Vehicle.class);
+    }
+
+    public VehicleDTO toVehicleDTO(Vehicle entity) {
+        return modelMapper.map(entity, VehicleDTO.class);
+    }
+
+    public List<VehicleDTO> toVehicleDTOList(List<Vehicle> vehicleList) {
+        return modelMapper.map(vehicleList, new TypeToken<List<VehicleDTO>>(){}.getType());
     }
 }
