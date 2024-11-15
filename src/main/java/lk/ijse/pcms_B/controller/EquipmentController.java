f0a26b25f9b73b439b6eda6372d85e2f2c49d940
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-15 08:01:10 +0530

    impl equipment end points

--- src/main/java/lk/ijse/pcms_B/controller/EquipmentController.java
+++ src/main/java/lk/ijse/pcms_B/controller/EquipmentController.java
@@ -1,34 +1,70 @@
 package lk.ijse.pcms_B.controller;
 
-import org.springframework.http.RequestEntity;
+import lk.ijse.pcms_B.customStatusCode.SelectedEquipmentErrorCode;
+import lk.ijse.pcms_B.dto.EquipmentStatus;
+import lk.ijse.pcms_B.dto.impl.EquipmentDTO;
+import lk.ijse.pcms_B.exception.DataPersistException;
+import lk.ijse.pcms_B.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/equipments")
 public class EquipmentController {
 
-    @GetMapping
-    public RequestEntity<Void> getEquipment() {
-        return null;
+    @Autowired
+    private EquipmentService service;
+
+    @GetMapping(value = "/{equipmentID}")
+    public EquipmentStatus getEquipment(@PathVariable("equipmentID") String ID) {
+        if (ID.isEmpty() || ID == null)
+            return new SelectedEquipmentErrorCode(1, "Equipment ID is Not Valid!");
+        return service.getEquipment(ID);
     }
 
     @GetMapping
-    public RequestEntity<Void> getAllEquipments() {
-        return null;
+    public List<EquipmentDTO> getAllEquipments() {
+        return service.getAllEquipment();
     }
 
     @PostMapping
-    public RequestEntity<Void> saveEquipment() {
-        return null;
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO dto) {
+        try {
+            service.saveEquipment(dto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping
-    public RequestEntity<Void> updateEquipment() {
-        return null;
+    @PutMapping(value = "/{equipmentID}")
+    public ResponseEntity<Void> updateEquipment(@PathVariable("equipmentID") String equipmentID, @RequestBody EquipmentDTO dto) {
+        try {
+            service.updateEquipment(equipmentID, dto);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @DeleteMapping
-    public RequestEntity<Void> deleteEquipment() {
-        return null;
+    @DeleteMapping(value = "/{equipmentID}")
+    public ResponseEntity<Void> deleteEquipment(@PathVariable("equipmentID") String equipmentID) {
+        try {
+            service.deleteEquipment(equipmentID);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
