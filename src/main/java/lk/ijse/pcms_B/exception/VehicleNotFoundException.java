58356cb444d094361c848ca636c8cd3ac774033a
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-15 09:10:54 +0530

    impl custom exception for vehicle

---
+++ src/main/java/lk/ijse/pcms_B/exception/VehicleNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.pcms_B.exception;
+
+public class VehicleNotFoundException extends RuntimeException{
+    public VehicleNotFoundException() {
+        super();
+    }
+
+    public VehicleNotFoundException(String message) {
+        super(message);
+    }
+
+    public VehicleNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
