9c781ab330e9a30ebb06366a59fc0546ca09173d
nuharaomesh <omeshnuhara4@gmail.com>
2024-11-15 09:10:25 +0530

    impl vehicle end points

--- src/main/java/lk/ijse/pcms_B/controller/VehicleController.java
+++ src/main/java/lk/ijse/pcms_B/controller/VehicleController.java
@@ -1,34 +1,76 @@
 package lk.ijse.pcms_B.controller;
 
+import lk.ijse.pcms_B.customStatusCode.SelectedVehicleErrorCode;
+import lk.ijse.pcms_B.dto.VehicleStatus;
+import lk.ijse.pcms_B.dto.impl.VehicleDTO;
+import lk.ijse.pcms_B.exception.DataPersistException;
+import lk.ijse.pcms_B.exception.VehicleNotFoundException;
+import lk.ijse.pcms_B.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/vehicles")
 public class VehicleController {
 
+    @Autowired
+    private VehicleService vehicleService;
+
     @GetMapping(value = "/{vehicleCode}")
-    public ResponseEntity<Void> getVehicle(@PathVariable("vehicleCode") String vehicleCode) {
-        return null;
+    public VehicleStatus getVehicle(@PathVariable("vehicleCode") String vehicleCode) {
+        if (vehicleCode.isEmpty() || vehicleCode == null) {
+            return new SelectedVehicleErrorCode(1, "Vehicle CODE is Not Valid!");
+        }
+        return vehicleService.getVehicle(vehicleCode);
     }
 
     @GetMapping
-    public ResponseEntity<Void> getAllVehicles() {
-        return null;
+    public List<VehicleDTO> getAllVehicles() {
+        return vehicleService.getAllVehicles();
     }
 
     @PostMapping
-    public ResponseEntity<Void> saveVehicle() {
-        return null;
+    public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO dto) {
+        try {
+            vehicleService.saveVehicle(dto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<Void> updateVehicle() {
-        return null;
+    @PutMapping(value = "/{vehicleCode}")
+    public ResponseEntity<Void> updateVehicle(@PathVariable("vehicleCode") String vehicleCode, @RequestBody VehicleDTO dto) {
+        try {
+            vehicleService.updateVehicle(vehicleCode, dto);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> deleteVehicle() {
-        return null;
+    @DeleteMapping(value = "/{vehicleCode}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable("vehicleCode") String vehicleCode) {
+        try {
+            vehicleService.deleteVehicle(vehicleCode);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (VehicleNotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
